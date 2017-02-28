0f02cbb9078cd7baaa4c1bde44fb5d849ff0f49c
Adel Mahjoub <dragonhunter250@gmail.com>
2017-02-28 21:50:09 +0100

    updated readme, added more key: values to json response

--- data.js
+++ data.js
@@ -4,13 +4,16 @@ var http = require("http");
 var dns = require("dns");
 
 module.exports = function(req, res, next) {
-   var info = {}; // data to send
+  var info = {}; // data to send
+  info.ip = req.ip;
+  info.language = req.get("Accept-Language").split(",")[0];
+  info.software = req.get("user-agent").match(/\(.+?\)/)[0];
+  info.user_agent = req.get("user-agent");
   // Get GeoIP info
   http.get("http://freegeoip.net/json/" + req.ip, function(response) {
     response.setEncoding('utf8');
     response.on("data", function(data) {
-      info = JSON.parse(data);
-      info.user_agent = req.get("user-agent");
+      info.location = JSON.parse(data);
       //Get ISP Info
       dns.reverse(req.ip, function(err, domain) {
         if(err) {
