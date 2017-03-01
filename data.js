ea74fa5074fa4571a2cdede6941fba5befe4f5a3
Adel Mahjoub <dragonhunter250@gmail.com>
2017-03-01 07:45:42 +0100

    testing

--- data.js
+++ data.js
@@ -6,6 +6,7 @@ var dns = require("dns");
 module.exports = function(req, res, next) {
   var info = {}; // data to send
   info.ip = req.ip;
+  console.log(req.ip)
   info.language = req.get("Accept-Language").split(",")[0];
   info.software = req.get("user-agent").match(/\(.+?\)/)[0];
   info.user_agent = req.get("user-agent");
